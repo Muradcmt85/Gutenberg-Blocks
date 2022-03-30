b069eb5dbd1125378f50626d993a61e764df1b73
Muradcmt85 <engrmuradcmt@gmail.com>
2022-03-30 16:09:13 +0600

    Gutenberg Block Description Changed

--- gutenbergblocks.php
+++ gutenbergblocks.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:       Gutenberg Blocks
- * Description:       Example block written with ESNext standard and JSX support – build step required.
+ * Description:       Gutenberg Block Creation for Custom Modules.
  * Requires at least: 5.8
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Md Murad Hossain
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       gutenbergblocks
